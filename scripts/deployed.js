bd94494bf5d418af9a2942b577db0ac6b8427188
Mac <MacRen000@gmail.com>
2023-04-18 17:17:49 +0800

    redeploy contracts and update address of zksync testnet

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -1,10 +1,10 @@
 module.exports = {
     dev: {
-        swapX2YModule: '0x47798d598891708ceD5Bd425b9bf3C56AfA473F0',
-        swapY2XModule: '0xc23Daa069A7F42818dd43FB0c2c3497c3e12da78',
-        liquidityModule: '0x1a3CC65Fe937C7dDd110cB65b281D7075766CA02',
-        limitOrderModule: '0x7FD55801c066AeB0bD848c2BA8AEc821AF700A41',
-        flashModule: '0x9A0C674c7FC173d3680f7b889CBB9a8570C5Ce73',
+	swapX2YModule: '0x19ed8bB72F93B87A0605fAcc116019039757e95A',
+	swapY2XModule: '0x4A7Df45560899606f1fa5aE7475816ecBbf66A68',
+	liquidityModule: '0x38D526f278189Cb6983Cf8bc58BBFAea7D2c3B22',
+	limitOrderModule: '0x47798d598891708ceD5Bd425b9bf3C56AfA473F0',
+	flashModule: '0x1a3CC65Fe937C7dDd110cB65b281D7075766CA02',
         factory:'',
     },
     prod: {
